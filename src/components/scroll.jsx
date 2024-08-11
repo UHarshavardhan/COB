9916795e8621e886bd6386936c404bda3a8abe39
u harshavardhan <harshavardhan@us-MacBook-Air.local>
2024-08-11 22:05:10 +0530

    added scroll affect

--- src/components/scroll.jsx
+++ src/components/scroll.jsx
@@ -1,54 +1,74 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import google from '../images/Google.png';
 
-function scroll(){
+function Scroll() {
+    const scrollRef = useRef(null);
+
+    useEffect(() => {
+        const scrollInterval = setInterval(() => {
+            if (scrollRef.current) {
+                scrollRef.current.scrollBy({
+                    left: 570, 
+                    behavior: "smooth",
+                });
+            }
+        }, 3000); 
+
+        return () => clearInterval(scrollInterval);
+    }, []);
 
     return (
-<div className="flex snap-x space-x-6 py-5 scroll-smooth w-full overflow-x-scroll">
-<div className="snap-center flex flex-col border-white shadow-xl w-[567px] h-[400px] border-2 rounded-2xl shadow-slate-300 mx-2">
-  <div className="flex justify-center pt-4">
-    <img src={google} alt="google" className="w-[150px] h-[47px]" />
-  </div>
-  <div className="flex justify-center px-4 py-4">
-    <span className="text-[24px] text-center">
-      I’m a law student from National Law School of India University, Banglore. I wanted to get admission in university to get the best skills and education, but I was confused with so many options available, but Guide Me Career helped me to get the best course and university in the line of education which could make me successful ahead.
-    </span>
-  </div>
-</div>
-{/* Repeat for other cards */}
-<div className="snap-center flex flex-col border-white shadow-xl w-[567px] h-[400px] border-2 rounded-2xl shadow-slate-300 mx-2">
-  <div className="flex justify-center pt-4">
-    <img src={google} alt="google" className="w-[150px] h-[47px]" />
-  </div>
-  <div className="flex justify-center px-4 py-4">
-    <span className="text-[24px] text-center">
-      I’m a law student from National Law School of India University, Banglore. I wanted to get admission in university to get the best skills and education, but I was confused with so many options available, but Guide Me Career helped me to get the best course and university in the line of education which could make me successful ahead.
-    </span>
-  </div>
-</div>
-<div className="snap-center flex flex-col border-white shadow-xl w-[567px] h-[400px] border-2 rounded-2xl shadow-slate-300 mx-2">
-  <div className="flex justify-center pt-4">
-    <img src={google} alt="google" className="w-[150px] h-[47px]" />
-  </div>
-  <div className="flex justify-center px-4 py-4">
-    <span className="text-[24px] text-center">
-      I’m a law student from National Law School of India University, Banglore. I wanted to get admission in university to get the best skills and education, but I was confused with so many options available, but Guide Me Career helped me to get the best course and university in the line of education which could make me successful ahead.
-    </span>
-  </div>
-</div>
-<div className="snap-center flex flex-col border-white shadow-xl w-[567px] h-[400px] border-2 rounded-2xl shadow-slate-300 mx-2">
-  <div className="flex justify-center pt-4">
-    <img src={google} alt="google" className="w-[150px] h-[47px]" />
-  </div>
-  <div className="flex justify-center px-4 py-4">
-    <span className="text-[24px] text-center">
-      I’m a law student from National Law School of India University, Banglore. I wanted to get admission in university to get the best skills and education, but I was confused with so many options available, but Guide Me Career helped me to get the best course and university in the line of education which could make me successful ahead.
-    </span>
-  </div>
-</div>
-{/* Add more cards as needed */}
-</div>
-    )
+        <div className="w-full h-full overflow-x-auto" ref={scrollRef}>
+            <div className="flex space-x-6 snap-x scroll-smooth">
+                <div className="snap-center flex-shrink-0 flex flex-col border-white shadow-2xl w-[567px] h-[400px] rounded-2xl shadow-slate-300 mx-2 bg-white">
+                    <div className="flex justify-center pt-4">
+                        <img src={google} alt="google" className="w-[150px] h-[47px]" />
+                    </div>
+                    <div className="flex justify-center px-4 py-4">
+                        <span className="text-[24px] text-center">
+                            I’m a law student from National Law School of India University, Banglore. I wanted to get admission in university to get the best skills and education, but I was confused with so many options available, but Guide Me Career helped me to get the best course and university in the line of education which could make me successful ahead.
+                        </span>
+                    </div>
+                </div>
+
+                {/* Pink Background for alternate card */}
+                <div className="snap-center flex-shrink-0 flex flex-col border-white shadow-xl w-[567px] h-[400px]  rounded-2xl shadow-slate-300 mx-2 bg-[#E9D6F9]">
+                    <div className="flex justify-center pt-4">
+                        <img src={google} alt="google" className="w-[150px] h-[47px]" />
+                    </div>
+                    <div className="flex justify-center px-4 py-4">
+                        <span className="text-[24px] text-center">
+                            I’m a law student from National Law School of India University, Banglore. I wanted to get admission in university to get the best skills and education, but I was confused with so many options available, but Guide Me Career helped me to get the best course and university in the line of education which could make me successful ahead.
+                        </span>
+                    </div>
+                </div>
+
+                <div className="snap-center flex-shrink-0 flex flex-col border-white shadow-xl w-[567px] h-[400px] border-2 rounded-2xl shadow-slate-300 mx-2 bg-white">
+                    <div className="flex justify-center pt-4">
+                        <img src={google} alt="google" className="w-[150px] h-[47px]" />
+                    </div>
+                    <div className="flex justify-center px-4 py-4">
+                        <span className="text-[24px] text-center">
+                            I’m a law student from National Law School of India University, Banglore. I wanted to get admission in university to get the best skills and education, but I was confused with so many options available, but Guide Me Career helped me to get the best course and university in the line of education which could make me successful ahead.
+                        </span>
+                    </div>
+                </div>
+
+                {/* Pink Background for alternate card */}
+                <div className="snap-center flex-shrink-0 flex flex-col border-white shadow-xl w-[567px] h-[400px] rounded-2xl shadow-slate-300 mx-2 bg-[#E9D6F9
+]">
+                    <div className="flex justify-center pt-4">
+                        <img src={google} alt="google" className="w-[150px] h-[47px]" />
+                    </div>
+                    <div className="flex justify-center px-4 py-4">
+                        <span className="text-[24px] text-center">
+                            I’m a law student from National Law School of India University, Banglore. I wanted to get admission in university to get the best skills and education, but I was confused with so many options available, but Guide Me Career helped me to get the best course and university in the line of education which could make me successful ahead.
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default scroll;
+export default Scroll;
